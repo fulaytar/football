b6ca37294bed6110a384f091bfb3688718c59aea
fulaytar <fulaytar2405@gmail.com>
2024-01-28 00:13:57 +0200

    дороби

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ const pole = document.getElementById('pole');
 const ball = document.getElementById('ball');
 console.dir(pole);
 console.dir(ball)
-function getPosition(event) {
+ function getPosition(event) {
     let x = event.clientX- pole.offsetLeft;
     let y = event.clientY- pole.offsetTop;
     console.log(event.clientX, event.clientY,"тут х та у");
     ball.style.top = y - ball.offsetHeight / 2 + 'px';
     ball.style.left = x - ball.offsetWidth / 2 + 'px';
 }
-pole.addEventListener("click",getPosition)
+pole.addEventListener("click",getPosition) 
