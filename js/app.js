7423669fe8c85e2bbfdfad4e30c915f17f8d01f1
fulaytar <fulaytar2405@gmail.com>
2024-01-28 00:31:12 +0200

    дороби потім

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ const pole = document.getElementById('pole');
 const ball = document.getElementById('ball');
 console.dir(pole);
 console.dir(ball)
+let degBall = 360; 
  function getPosition(event) {
     let x = event.clientX- pole.offsetLeft;
-    let y = event.clientY- pole.offsetTop;
+    let y = event.clientY - pole.offsetTop;
+    degBall += 360;
     console.log(event.clientX, event.clientY,"тут х та у");
     ball.style.top = y - ball.offsetHeight / 2 + 'px';
     ball.style.left = x - ball.offsetWidth / 2 + 'px';
+    ball.style.transform = 'rotate(' + degBall + 'deg)';
 }
 pole.addEventListener("click",getPosition) 
